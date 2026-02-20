ef7579229766676154e53e80df3ccde1a09cd2f7
Simrndeep Singh <simrndeepsingh@gmail.com>
2026-02-20 16:38:18 +0530

    fix: semantic-git and changelog added

--- release.config.cjs
+++ release.config.cjs
@@ -3,8 +3,24 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+
     '@semantic-release/npm',
+
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]'
+      }
+    ],
+
     '@semantic-release/github'
   ]
 };
